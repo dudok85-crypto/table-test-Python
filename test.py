f2e4f100f2970e120a32218ccfd95d0d5f5fc2e7
foga.wk <dudok85@gmail.com>
2025-11-25 13:10:51 +0300

    Update test.py

--- test.py
+++ test.py
@@ -3,4 +3,10 @@ from prettytable import PrettyTable
 receipt = PrettyTable()
 receipt.field_names = ["№", 'Продукт',"цена", 'Количество', 'стоимость']
 
+for i in range(3):
+    product = input("укажите название продукта:")
+    price = int(input("укажите цену:"))
+    col = int(input("укажите количество:"))
+    receipt.add_row([i+1, product, price, col, price*col])
+
 print(receipt)
