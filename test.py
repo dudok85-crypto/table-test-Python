856a87349086b51e538c1e8e4c748f3ec452d2c8
foga.wk <dudok85@gmail.com>
2025-12-02 12:49:57 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from prettytable import PrettyTable
 import matplotlib.pyplot as plt
 
+#создание таблицы и шапки для неё.
 receipt = PrettyTable()
 receipt.field_names = ["№", 'Продукт',"цена", 'Количество', 'стоимость']
 
@@ -8,11 +9,12 @@ receipt.field_names = ["№", 'Продукт',"цена", 'Количество
 prod = [] 
 pay = []
 
+
 for i in range(3):
     product = input("укажите название продукта:")
     price = int(input("укажите цену:"))
     col = int(input("укажите количество:"))
-    receipt.add_row([i+1, product, price, col, price*col])
+    receipt.add_row([i+1, product, price, col, price*col]) # добавление всего нужного в саму таблицу
     prod.append(product)
     pay.append(price*col)
 
